6e909402a656c636cbffbc8ffed0caffbce23ac9
Gemma Hentsch <gemma@halfapenguin.com>
2019-01-28 09:41:07 +0000

    The intial shop model file

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    url = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+    tags = models.ManyToManyField(Tag)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    show_on_website = models.BooleanField(default=False)
