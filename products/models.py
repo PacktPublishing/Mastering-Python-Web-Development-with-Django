2ca9f6bf36510fb682a06777387b9b152e03d6d4
Gemma Hentsch <gemma@halfapenguin.com>
2019-01-31 23:06:36 +0000

    string representation method on tag

--- products/models.py
+++ products/models.py
@@ -7,6 +7,9 @@ class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Item(models.Model):
     name = models.CharField(max_length=100, unique=True)
