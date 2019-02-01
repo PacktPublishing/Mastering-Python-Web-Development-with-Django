b7b97bde7773b023e3052deedd5da9fe72fcb455
Gemma Hentsch <gemma@halfapenguin.com>
2019-02-01 19:40:19 +0000

    prepopulated field in TagAdmin

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Tag
 
-admin.site.register(Tag)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
