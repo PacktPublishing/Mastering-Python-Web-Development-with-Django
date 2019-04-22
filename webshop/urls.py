fea20942f211b402f28e2a9feb0461e5b358df05
Gemma Hentsch <gemma@halfapenguin.com>
2019-04-22 17:44:26 +0100

    adding the simple view into urls file

--- webshop/urls.py
+++ webshop/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views as product_views
 
 urlpatterns = [
+    path('simple/', product_views.simple_view),
     path('admin/', admin.site.urls),
 ]
