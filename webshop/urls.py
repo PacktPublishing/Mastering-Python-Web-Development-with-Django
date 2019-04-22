8e0a3e46dd4bce10f44347bcd98541cd1aa20a2d
Gemma Hentsch <gemma@halfapenguin.com>
2019-04-22 19:34:20 +0100

    item detail passing in slug

--- webshop/urls.py
+++ webshop/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from products import views as product_views
 
 urlpatterns = [
+    path('item/detail/<slug:item_slug>/', product_views.item_detail),
     path('simple/', product_views.simple_view),
     path('admin/', admin.site.urls),
 ]
