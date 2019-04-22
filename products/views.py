8c20bb9fe16b634d5123149066a89fb05dd9df89
Gemma Hentsch <gemma@halfapenguin.com>
2019-04-22 22:11:59 +0100

    Reorder the views

--- products/views.py
+++ products/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
 
-def simple_view(request):
-	return HttpResponse("Hello World")
 
 def item_detail(request, item_slug):
 	item = Item.objects.get(slug=item_slug)
 	output_string = "Page for item \"{}\"".format(item.name)
 	return HttpResponse(output_string)
+
+
+def simple_view(request):
+	return HttpResponse("Hello World")
