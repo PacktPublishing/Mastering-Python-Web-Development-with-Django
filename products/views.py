e54bd67a35b7ca072652f5451c6a009b650cc521
Gemma Hentsch <gemma@halfapenguin.com>
2019-04-22 22:13:59 +0100

    get_object_or_404

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Item
 
 
 def item_detail(request, item_slug):
-	item = Item.objects.get(slug=item_slug)
+	item = get_object_or_404(Item, slug=item_slug)
 	output_string = "Page for item \"{}\"".format(item.name)
 	return HttpResponse(output_string)
 
