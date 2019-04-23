03bbd986fc286031bb8284eb82666dbde980ea83
Gemma Hentsch <gemma@halfapenguin.com>
2019-04-23 09:02:21 +0100

    render with context

--- products/views.py
+++ products/views.py
@@ -5,8 +5,10 @@ from .models import Item
 
 def item_detail(request, item_slug):
 	item = get_object_or_404(Item, slug=item_slug)
-	output_string = "Page for item \"{}\"".format(item.name)
-	return render(request, "products/item_detail.html")
+	context = {
+		"item": item,
+	}
+	return render(request, "products/item_detail.html", context)
 
 
 def simple_view(request):
