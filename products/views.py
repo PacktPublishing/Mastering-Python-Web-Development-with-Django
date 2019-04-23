df8c4d29e7cd8187d78e7f549184c419c05df520
Gemma Hentsch <gemma@halfapenguin.com>
2019-04-23 08:36:47 +0100

    view now loads template

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .models import Item
 def item_detail(request, item_slug):
 	item = get_object_or_404(Item, slug=item_slug)
 	output_string = "Page for item \"{}\"".format(item.name)
-	return HttpResponse(output_string)
+	return render(request, "products/item_detail.html")
 
 
 def simple_view(request):
