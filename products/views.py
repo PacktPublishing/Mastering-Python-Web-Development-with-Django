2a9805bc64eb8de4430a901c3f8cf35cbe6d41f2
Gemma Hentsch <gemma@halfapenguin.com>
2019-05-07 22:33:56 +0100

    Import tag into views

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.http import HttpResponse
-from .models import Item
+from .models import Item, Tag
 
 class ItemListView(ListView):
 	context_object_name = 'items'
