9f9d3e2463b3d5686b65021fbf039b228f133ab2
Gemma Hentsch <gemma@halfapenguin.com>
2019-05-07 06:27:16 +0100

    item_tag name

--- webshop/urls.py
+++ webshop/urls.py
@@ -4,7 +4,7 @@ from products import views as product_views
 
 urlpatterns = [
 	path('item/', product_views.ItemListView.as_view()),
-	path('item/tag/<slug:tag_slug>/', product_views.TaggedItemListView.as_view()),
+	path('item/tag/<slug:tag_slug>/', product_views.TaggedItemListView.as_view(), name='item_tag'),
     path('item/detail/<slug:item_slug>/', product_views.item_detail, name='item_detail'),
     path('simple/', product_views.simple_view),
     path('admin/', admin.site.urls),
