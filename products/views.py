aec45c3f352875488f3756bc8463157c3e4359fd
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-05 15:01:36 +0100

    assert tag self.kwargs

--- products/views.py
+++ products/views.py
@@ -4,25 +4,26 @@ from django.http import HttpResponse
 from .models import Item, Tag
 
 class ItemListView(ListView):
-	context_object_name = 'items'
-	model = Item
+    context_object_name = 'items'
+    model = Item
 
 
 class TaggedItemListView(ItemListView):
-	template_name = "products/item_tag.html"
+    template_name = "products/item_tag.html"
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		return context
+    def get_context_data(self, **kwargs):
+        assert False, self.kwargs
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 def item_detail(request, item_slug):
-	item = get_object_or_404(Item, slug=item_slug)
-	context = {
-		"item": item,
-	}
-	return render(request, "products/item_detail.html", context)
+    item = get_object_or_404(Item, slug=item_slug)
+    context = {
+        "item": item,
+    }
+    return render(request, "products/item_detail.html", context)
 
 
 def simple_view(request):
-	return HttpResponse("Hello World")
+    return HttpResponse("Hello World")
