7da8a4282905e66323cbfd17e6fef078681791fb
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-16 12:10:05 +0100

    search view in urls

--- webshop/urls.py
+++ webshop/urls.py
@@ -4,6 +4,7 @@ from products import views as product_views
 
 urlpatterns = [
 	path('item/', product_views.ItemListView.as_view()),
+	path('item/search/', product_views.SearchItemListView.as_view(), name='item_search'),
 	path('item/tag/<slug:tag_slug>/', product_views.TaggedItemListView.as_view(), name='item_tag'),
     path('item/detail/<slug:item_slug>/', product_views.item_detail, name='item_detail'),
     path('simple/', product_views.simple_view),
