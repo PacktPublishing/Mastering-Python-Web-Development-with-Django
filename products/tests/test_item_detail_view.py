0b6dd6d7702030b1863332ee71354822082fa57c
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-27 15:37:40 +0100

    initial test boilerplate

--- products/tests/test_item_detail_view.py
+++ products/tests/test_item_detail_view.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+
+
+class TestItemDetailView(TestCase):
+    def test_you_can_access_an_item(self):
+        """
+        You can access an item including expected content and tags
+        """
+        pass
