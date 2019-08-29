6c37c61b26c6b07a17c40fc0360364056a22c7e1
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-29 11:17:14 +0100

    check if tag_url is on page

--- products/tests/test_item_detail_view.py
+++ products/tests/test_item_detail_view.py
@@ -13,3 +13,4 @@ class TestItemDetailView(TestCase):
         self.assertTrue("Top Hat (VTI)" in str(response.content))
         self.assertTrue("A fancy hat for formal occasions" in str(response.content))
         tag_url = reverse('item_tag', kwargs={'tag_slug': 'stylish-hats-tag'})
+        self.assertTrue(tag_url in str(response.content))
