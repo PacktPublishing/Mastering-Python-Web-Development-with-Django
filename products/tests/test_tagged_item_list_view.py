1912220752df0f09bd493189225e9c8cee67f85b
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-30 14:34:16 +0100

    empty test can see tagged items view

--- products/tests/test_tagged_item_list_view.py
+++ products/tests/test_tagged_item_list_view.py
@@ -3,3 +3,8 @@ from django.urls import reverse
 
 class TestTaggedItemListView(TestCase):
     fixtures = ["products/tests/fixtures/basic.json"]
+
+    def test_that_you_can_see_tagged_items_in_the_list(self):
+        client = Client()
+        url = reverse('item_tag', kwargs={'tag_slug':'stylish-hats-tag'})
+        response = client.get(url)
