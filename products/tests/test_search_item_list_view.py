83a9533b53c53885dd398456f30f2d5b84623a3a
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-30 21:12:30 +0100

    start of the test_that_you_do_not_get_bad_results_when_finding_items_by_name

--- products/tests/test_search_item_list_view.py
+++ products/tests/test_search_item_list_view.py
@@ -12,3 +12,8 @@ class TestSearchItemListView(TestCase):
         self.assertIn("Walking Cane (VUTI)", str(response.content))
         walking_cane_url = reverse("item_detail", kwargs={"item_slug": "walking-cane-vuti"})
         self.assertIn(walking_cane_url, str(response.content))
+
+    def test_that_you_do_not_get_bad_results_when_finding_items_by_name(self):
+        client = Client()
+        url = reverse('item_search')
+        response = client.get(url, {"q": "cane"})
