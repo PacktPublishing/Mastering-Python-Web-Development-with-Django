c401c8dac8a3f5fc057943eb14a6580d75c54543
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-30 08:28:16 +0100

    assert content of item_list view in test

--- products/tests/test_item_list_view.py
+++ products/tests/test_item_list_view.py
@@ -3,3 +3,8 @@ from django.urls import reverse
 
 class TestItemListView(TestCase):
     fixtures = ["products/tests/fixtures/basic.json"]
+    def test_that_you_can_see_items_in_the_list(self):
+        client = Client()
+        url = reverse('item_list')
+        response = client.get(url)
+        self.assertFalse(response.content)
