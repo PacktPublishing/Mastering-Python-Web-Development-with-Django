7c9cb0ebeb0925a4fe74de2e4aef08684e13cb12
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-30 20:26:41 +0100

    first search query test

--- products/tests/test_search_item_list_view.py
+++ products/tests/test_search_item_list_view.py
@@ -4,3 +4,8 @@ from django.urls import reverse
 
 class TestSearchItemListView(TestCase):
     fixtures = ["products/tests/fixtures/basic.json"]
+
+    def test_that_you_can_find_items_by_name(self):
+        client = Client()
+        url = reverse('item_search')
+        response = client.get(url, {"q": "cane"})
