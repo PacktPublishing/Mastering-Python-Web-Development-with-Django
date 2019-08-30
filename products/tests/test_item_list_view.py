9b129944943cf8d50fe7af7664e4164793dcf516
Gemma Hentsch <gemma@halfapenguin.com>
2019-08-30 12:35:12 +0100

    test_that_you_cannot_see_invisible_items_in_the_list test

--- products/tests/test_item_list_view.py
+++ products/tests/test_item_list_view.py
@@ -13,3 +13,11 @@ class TestItemListView(TestCase):
         self.assertIn("Walking Cane (VUTI)", str(response.content))
         walking_cane_url = reverse("item_detail", kwargs={"item_slug": "walking-cane-vuti"})
         self.assertIn(walking_cane_url, str(response.content))
+
+    def test_that_you_cannot_see_invisible_items_in_the_list(self):
+        client = Client()
+        url = reverse('item_list')
+        response = client.get(url)
+        self.assertNotIn("Baseball Cap (ITI)", str(response.content))
+        baseball_url = reverse("item_detail", kwargs={"item_slug": "baseball-cap-iti"})
+        self.assertNotIn(baseball_url, str(response.content))
